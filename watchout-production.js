94d970b3ec0420887573ab3c5a8ef41c3c534739
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:19 +0000

    bulk replace system.emit calls

--- watchout-production.js
+++ watchout-production.js
@@ -114,7 +114,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 		'run': {
 			label: 'Run',
 			options: [{

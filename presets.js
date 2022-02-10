1249cc6bbdfbee2a6fb447ec7af6e9479a5c629c
Alberto Righetto <arighetto88@gmail.com>
2022-02-10 10:39:00 +0100

    Add feedbacks and presets files

---
+++ presets.js
@@ -0,0 +1,22 @@
+module.exports = {
+	getPresets() {
+    var self = this;
+    let presets = [];
+
+    presets.push({
+      category: 'Commands',
+      label: 'Update timeline list',
+      bank: {
+        style: 'text',
+        text: 'Update timeline list',
+        color: self.rgb(255,255,255),
+        bgcolor: self.rgb(0,0,0)
+      },
+      actions: [{
+        action: 'getAuxTimelines'
+      }]
+    });
+
+		return presets;
+	}
+}
